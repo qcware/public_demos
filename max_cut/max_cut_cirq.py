50d9a8cafeedad6fa9fe924029318a9c869ac145
Joshua Scherschel <josh.scherschel@qcware.com>
2021-07-07 11:21:47 -0700

    add notice that cirq has been replaced by bin opt/qaoa

---
+++ max_cut/max_cut_cirq.py
@@ -0,0 +1,4 @@
+"""
+Please note that this functionality has been replaced by QC Ware's Binary Optimization and QAOA offerings.
+See https://forge.qcware.com/data_science/#binary_optimization for more details.
+"""
